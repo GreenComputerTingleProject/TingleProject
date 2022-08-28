a8a04d3980407ee13ffc24e97cb14e35dd3af16d
Changyong Oh <cyong9573@naver.com>
2022-08-28 20:20:48 +0900

    0828 정기결제, 멤버쉽 해지 추가

--- src/main/java/model/PayDTO.java
+++ src/main/java/model/PayDTO.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class PayDTO {
 
     int id, user_id, paid_amount;
-    String imp_uid;
+    String imp_uid, customer_uid, merchant_uid;
     Date reg_date;
 
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 (E) HH:mm:ss");
@@ -15,11 +15,13 @@ public class PayDTO {
 
     }
 
-    public PayDTO(int id, int user_id, int paid_amount, String imp_uid, Date reg_date, SimpleDateFormat sdf) {
+    public PayDTO(int id, int user_id, int paid_amount, String imp_uid, String customer_uid, String merchant_uid, Date reg_date, SimpleDateFormat sdf) {
         this.id = id;
         this.user_id = user_id;
         this.paid_amount = paid_amount;
         this.imp_uid = imp_uid;
+        this.customer_uid = customer_uid;
+        this.merchant_uid = merchant_uid;
         this.reg_date = reg_date;
         this.sdf = sdf;
     }
@@ -54,6 +56,22 @@ public class PayDTO {
 
     public void setImp_uid(String imp_uid) {
         this.imp_uid = imp_uid;
+    }
+
+    public String getCustomer_uid() {
+        return customer_uid;
+    }
+
+    public void setCustomer_uid(String customer_uid) {
+        this.customer_uid = customer_uid;
+    }
+
+    public String getMerchant_uid() {
+        return merchant_uid;
+    }
+
+    public void setMerchant_uid(String merchant_uid) {
+        this.merchant_uid = merchant_uid;
     }
 
     public Date getReg_date() {
